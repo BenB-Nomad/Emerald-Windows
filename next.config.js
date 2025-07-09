e1202c7ee105f5895eb0b1aebde9e87deeef82f2
BenB-Nomad <benbarrett@nomadai.co>
2025-07-09 15:39:50 +0100

    Fix build issues - remove problematic webpack config

--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  // Remove any problematic configurations
-  webpack: (config) => {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-    };
-    return config;
-  },
 }
 
 module.exports = nextConfig 
